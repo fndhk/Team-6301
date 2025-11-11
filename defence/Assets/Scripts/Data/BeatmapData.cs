6057c428baa9017958dfeaa6e3eff8eb7d4654c7
fndhk <kwakyoungmin14@naver.com>
2025-11-11 13:46:54 +0900

    11/11

--- defence/Assets/Scripts/Data/BeatmapData.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-using UnityEngine;
-using static NoteSpawner;
-
-[CreateAssetMenu(fileName = "New Beatmap", menuName = "TowerDefense/Beatmap Data")]
-public class BeatmapData : ScriptableObject
-{
-    public List<NotePatternData> patterns = new List<NotePatternData>();
-}
